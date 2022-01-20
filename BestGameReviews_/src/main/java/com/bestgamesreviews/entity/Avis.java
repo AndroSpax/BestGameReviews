6bd72ecd63758b731d98f95ef7e9d1057b8c48dd
AndroSpax <63066048+AndroSpax@users.noreply.github.com>
2022-01-20 23:09:48 +0100

    Initialisation
    -.gitignore updated
    -modif des noms des packages

---
+++ BestGameReviews_/src/main/java/com/bestgamesreviews/entity/Avis.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.bestgamesreviews.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author Administrator
+ *
+ */
+@Entity
+@Getter
+@Setter
+public class Avis {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+	@GenericGenerator(name = "native", strategy = "native")
+	private Long id;
+
+}
