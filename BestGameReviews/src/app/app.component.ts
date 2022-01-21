f30d954b4eb86c28550e360da5c07cf4b9763577
RudLeTerrible <rudolph.nativel@gmail.com>
2022-01-21 21:24:20 +0100

    angular accueil et material

--- BestGameReviews/src/app/app.component.ts
+++ BestGameReviews/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'BestGameReviews';
+  constructor(iconRegistry: MatIconRegistry){
+
+  }
 }
+
