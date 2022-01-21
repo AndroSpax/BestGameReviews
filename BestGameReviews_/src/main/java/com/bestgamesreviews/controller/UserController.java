a39002557caf73bf007d857d165d6e4a27deb795
rudolphnativel <rudolph.nativel@gmail.com>
2022-01-21 15:14:36 +0100

    controller user avis jeux init

---
+++ BestGameReviews_/src/main/java/com/bestgamesreviews/controller/UserController.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.bestgamesreviews.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Marielle Machael Rudolph 
+ *
+ */
+@RestController
+@RequestMapping("api/users")
+public class UserController {
+
+	public UserController() {
+		// TODO Auto-generated constructor stub
+	}
+}
