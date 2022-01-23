8a7c1e5e0cebaec5d14cc71c2f439220f7ac1069
Marielle <marielle.bouchoux@gmail.com>
2022-01-23 16:26:18 +0100

    suppression projet ang

---
+++ BestGameReviews_M/BestGameReviews_Ang/src/app/avis/avis-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AjouterAvisComponent } from './ajouter-avis/ajouter-avis.component';
+// import { HomeComponent } from '../home/home.component';
+import { ListeAvisComponent } from './liste-avis/liste-avis.component';
+
+const routes: Routes = [
+  {path:'', redirectTo:'/avis/liste-avis', pathMatch:'full'},
+  {path:'liste-avis', component:ListeAvisComponent},
+  {path:'ajouter-avis', component:AjouterAvisComponent},
+];
+
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AvisRoutingModule { }
