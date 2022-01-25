7313eb701fd4f9c8fc286497dfa4c34392fa51eb
Marielle <marielle.bouchoux@gmail.com>
2022-01-25 10:40:33 +0100

    template inscription connexion

--- BestGameReviews_M/src/app/user/inscription/inscription.component.ts
+++ BestGameReviews_M/src/app/user/inscription/inscription.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/data.service';
+import { Subscription } from 'rxjs';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-inscription',
@@ -7,9 +11,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InscriptionComponent implements OnInit {
 
-  constructor() { }
 
+  // initialise le thème du user
+  theme: string = "";
+  subscription = new Subscription;
+
+  myForm: FormGroup;
+  email: AbstractControl;
+  tomorrow = new Date();
+
+
+  // pour la possibilité de voir le mot de passe que l'utilisateur saisie
+  hide = true;
+
+
+
+  constructor(private data: DataService, fb: FormBuilder) {
+    this.myForm = fb.group({
+      'email':['', Validators.required],
+ });
+    this.email= this.myForm.controls['email'];
+    this.tomorrow.setDate(this.tomorrow.getDate());
+   };
+
+  // onInit récupère le currentThème du data service
   ngOnInit(): void {
+    this.subscription = this.data.currentTheme.subscribe(theme => this.theme = theme)
+  }
+
+  ngOnDestroy(){
+    if (this.subscription != null){
+      this.subscription.unsubscribe();
+    }
+  }
+
+  // pour la validation du formulaire de connexion
+  onSubmit(value:string): void {
+    console.log('you submitted value:', value);
   }
 
 }
