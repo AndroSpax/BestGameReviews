f9c981af7cdfc008b06a71c183300df34310a63c
RudLeTerrible <rudolph.nativel@gmail.com>
2022-01-25 10:12:33 +0100

    correction du merge

--- BestGameReviews_/src/main/java/com/bestgamesreviews/dao/AvisDAO.java
+++ BestGameReviews_/src/main/java/com/bestgamesreviews/dao/AvisDAO.java
@@ -4,15 +4,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-import com.bestgamesreviews.entity.Joueur;
+import com.bestgamesreviews.entity.Avis;
+
 
 /**
- * Interface de persistence des entités Joueur
+ * Interface de persistence des entités Avis 
  * @author Marielle Michael Rudolph 
- *
+ * 
  */
-@Repository
 @RepositoryRestResource(exported = true)
-public interface JoueurDAO extends JpaRepository<Joueur, Long> {
+public interface AvisDAO extends JpaRepository<Avis, Long> {
 
 }
